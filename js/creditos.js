ac50ef5747247dbdfcf76a5b950060abe3fcb837
Erik <erikoliveira818@gmail.com>
2022-09-18 16:54:58 -0300

    Atualização das logos e script.

--- js/creditos.js
+++ js/creditos.js
@@ -1,86 +1,85 @@
 var posicao = 0;
-//1-2-3-4
-//<----\/
 
+/*
+    document.getElementById('name').innerText = "";
+    document.getElementById('foto').src="";
+    document.getElementById("linkedin").href = "";
+    document.getElementById("git").href = "";
+    document.getElementById("ig").href = "";
 
-function botaoVoltar() 
+    RAFA - ERIK - JOAO
+*/
+
+function botaoVoltar()
 {
     posicao -= 1;
+
+    if (posicao === -1) //abaixo do minimo
+    {
+        posicao = 2; //limite
+        joao()
+    }
+
     if (posicao === 1)
     {
-        document.getElementById('foto').src="imgs/autores/segundo.jpg"
-        document.getElementById("linkedin").href = "http://www.cnn.com/";
-        document.getElementById('name').innerText = "Nome2"
-        document.getElementById('funcao').innerText = "Função2"
+        erik()
     }
 
-    if (posicao === 2)
+    if (posicao === 0)
     {
-        document.getElementById('foto').src="imgs/autores/terceiro.png"
-        document.getElementById('name').innerText = "Nome3"
-        document.getElementById('funcao').innerText = "Função3"
+        rafa()
     }
-
-    if (posicao === 3)
-    {
-        document.getElementById('foto').src="imgs/autores/quarto.jpg"
-        document.getElementById('name').innerText = "Nome4"
-        document.getElementById('funcao').innerText = "Função4"
-    }
-
-    if(posicao ===-1)
-    {
-        posicao=3
-        document.getElementById('foto').src="imgs/autores/quarto.jpg"
-        document.getElementById('name').innerText = "Nome4"
-        document.getElementById('funcao').innerText = "Função4"
-    }
-
-    if(posicao ===0)
-    {
-        document.getElementById('foto').src="imgs/autores/primeiro.jpg"
-        document.getElementById('name').innerText = "Nome1"
-        document.getElementById('funcao').innerText = "Função1"
-    }
-    
 }
 
-function botaoAvancar() 
+function botaoAvancar()
 {
-    posicao++;
-
+    posicao++
+    
     if (posicao === 1)
     {
-        document.getElementById('foto').src="imgs/autores/segundo.jpg"
-        document.getElementById("linkedin").href = "http://www.cnn.com/";
-        document.getElementById('name').innerText = "Nome2"
-        document.getElementById('funcao').innerText = "Função2"
-
+        erik()
     }
 
-    if (posicao === 2)
+    if (posicao === 2) 
     {
-        document.getElementById('foto').src="imgs/autores/terceiro.png"
-        document.getElementById('name').innerText = "Nome3"
-        document.getElementById('funcao').innerText = "Função3"
+        joao()
     }
 
-    if (posicao === 3)
-    {
-        document.getElementById('foto').src="imgs/autores/quarto.jpg"
-        document.getElementById('name').innerText = "Nome4"
-        document.getElementById('funcao').innerText = "Função4"
-    }
-
-    if(posicao ===4)
+    if (posicao === 3)//Numero maximo
     {
         posicao = 0;
-        document.getElementById('foto').src="imgs/autores/primeiro.jpg"
-        document.getElementById('name').innerText = "Nome1"
-        document.getElementById('funcao').innerText = "Função1"
+        rafa()
     }
-    
 }
 
 
-//document.getElementById('foto').src="imgs/dispositivo.png";
+
+
+function rafa()
+{
+    document.getElementById('name').innerText = "Rafael Romariz";
+    document.getElementById('foto').src="imgs/autores/primeiro.jpg";
+    document.getElementById("linkedin").href = "http://linkedin.com/in/rafael-romariz-b2b45322b/";
+    document.getElementById("git").href = "http://github.com/IRMZI";
+    document.getElementById("ig").href = "http://instagram.com/Romariz.dev";
+}
+
+function erik()
+{
+    document.getElementById('name').innerText = "Erik Oliveira";
+    document.getElementById('foto').src="imgs/autores/segundo.jpg";
+    document.getElementById("linkedin").href = "https://www.linkedin.com/in/erikoliveira28";
+    document.getElementById("git").href = "https://github.com/SoL1dcs";
+    document.getElementById("ig").href = "http://instagram.com/erikoliveira818";
+}
+
+function joao() 
+{
+    document.getElementById('name').innerText = "João Zanardi";
+    document.getElementById('foto').src="imgs/autores/terceiro.png";
+    document.getElementById("linkedin").href = "";
+    document.getElementById("git").href = "";
+    document.getElementById("ig").href = ""; 
+}
+
+
