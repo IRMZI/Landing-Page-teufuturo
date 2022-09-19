0772bec442c17f727f3f720479d8a8f0435b5f35
Erik <erikoliveira818@gmail.com>
2022-09-18 23:54:26 -0300

    Adição das funções
    Ps: Adição da explicação sobre o creditos.js

--- js/creditos.js
+++ js/creditos.js
@@ -1,20 +1,17 @@
 var posicao = 0;
-/*
-    document.getElementById('name').innerText = "";
-    document.getElementById('foto').src="";
-    document.getElementById("linkedin").href = "";
-    document.getElementById("git").href = "";
-    document.getElementById("ig").href = "";
 
-    RAFA - ERIK - JOAO
-*/
 function botaoVoltar()
 {
     posicao -= 1;
 
-    if (posicao === -1) //abaixo do minimo
+    if (posicao === -1) //numero minimo
     {
-        posicao = 2; //limite
+        posicao = 3; 
+        lucas()
+    }
+
+    if (posicao === 2)
+    {
         joao()
     }
 
@@ -43,12 +40,18 @@ function botaoAvancar()
         joao()
     }
 
-    if (posicao === 3)//Numero maximo
+    if(posicao === 3)
+    {
+        lucas()
+    }
+
+    if (posicao === 4)//Numero maximo
     {
         posicao = 0;
         rafa()
     }
 }
+
 function rafa()
 {
     document.getElementById('name').innerText = "Rafael Romariz";
@@ -56,6 +59,7 @@ function rafa()
     document.getElementById("linkedin").href = "http://linkedin.com/in/rafael-romariz-b2b45322b/";
     document.getElementById("git").href = "http://github.com/IRMZI";
     document.getElementById("ig").href = "http://instagram.com/Romariz.dev";
+    document.getElementById('textoPesoal').innerText='Desenvolvedor front-end | UI/UX';
 }
 
 function erik()
@@ -65,6 +69,7 @@ function erik()
     document.getElementById("linkedin").href = "https://www.linkedin.com/in/erikoliveira28";
     document.getElementById("git").href = "https://github.com/SoL1dcs";
     document.getElementById("ig").href = "http://instagram.com/erikoliveira818";
+    document.getElementById('textoPesoal').innerText='Lider de desenvolvimento'
 }
 
 function joao() 
@@ -74,6 +79,28 @@ function joao()
     document.getElementById("linkedin").href = "";
     document.getElementById("git").href = "";
     document.getElementById("ig").href = ""; 
+    document.getElementById('textoPesoal').innerText="Desenvolvedor e Lider do squad";
+}
+
+function lucas()
+{
+    document.getElementById('name').innerText = "Lucas";
+    
+    document.getElementById('foto').src="imgs/autores/terceiro.png";
+    document.getElementById("linkedin").href = "";
+    document.getElementById("git").href = "";
+    document.getElementById("ig").href = "";
+    document.getElementById('textoPesoal').innerText="Designer e Scrum Master";
 }
 
 
+/*
+    Como funciona
+        Cada função possui as informações dos integrantes.
+        As funções botaoVoltar e botaoAvancar fazer o trabalho de chamar a função que vai mandar as informações para o HTML.
+    Manutenção
+        Caso seja necessario adionar novos integrantes, O "Numero maximo" devera ser almentado, o "numero minimo" devera ser reduzido
+        e sera necessario a criação de uma nova função com os dados do novo integrante.
+    Solução de erro
+        Caso aconteca de um integrante 'seja pulado' na navegação, verificar se a função dele está sendo chamada corretamente.
+*/
